122211354d216a7912486685a34e682c4fddf4da
rm17394 <mishrahul17@gmail.com>
2022-08-09 17:22:56 +0530

    make it better

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,7 @@ app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-//3:step heroku
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static("build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  })
-}
+
 
 app.listen(port, () => {
   console.log(`dailyNotes backend listening at http://localhost:${port}`)
